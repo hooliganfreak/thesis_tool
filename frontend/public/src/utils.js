a5d2bf2632a7335ef0871e41c2dfb7fd9abcbfe5
hooliganfreak <ivarsemi@arcada.fi>
2025-07-04 10:51:47 +0300

    Added settings menu with dark mode (not final)

--- frontend/public/src/utils.js
+++ frontend/public/src/utils.js
@@ -1,3 +1,34 @@
 // IN PROGRESS -----------------------------------------------------------------------------
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+// Settings button functionality
+export function settingsButton() {
+    const btn = document.getElementById('settingsBtn');
+    const modal = new bootstrap.Modal(document.getElementById('settingsModal'));
+
+    if (btn) {
+        btn.addEventListener('click', () => {
+            modal.show();
+        });
+    }
+}
+
+// Dark mode setting saves in localStorage
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+}
+
+// Darkmode toggle functionality
+darkModeToggle.addEventListener('change', () => {
+    console.log("check");
+    if (darkModeToggle.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled')
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+})
 
 // IN PROGRESS -----------------------------------------------------------------------------
